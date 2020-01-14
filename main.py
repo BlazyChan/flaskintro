e1918e91ba72e5ebbaace8a60775aad92c76112d
BlazyChan <gundarspelle@inbox.lv>
2020-01-14 11:33:06 +0000

    Add heroku files

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def about():
 @app.route('/contact')
 def contacts():
   return render_template('contact.html', phone = 12345678)
-
-app.run(host = '0.0.0.0', port = 8020)
+if __name__ == '__main__':
+  app.run(host = '0.0.0.0', port = 5000, threaded = True, debug = True)
 
 # push github un reģistrēties heroku.com
